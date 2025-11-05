f91c6bb885e542b08f41e56e0df74718f541ea09
auroraengelgeer <auroraengelgeer@hotmail.com>
2025-11-06 00:03:28 +0100

    made async function in App.jsx to display list of 20 pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import './App.css'
 import {Route, Routes} from "react-router-dom";
 import PokemonCard from "./components/PokemonCard/PokemonCard.jsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
+
+    const [pokemonList, setPokemonList] = useState([]);
+
+    useEffect(() => {
+        async function getPokemonData() {
+            try {
+                const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0');
+                setPokemonList(response.data.results)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        getPokemonData();
+
+    }, []);
+
 
     return (
         <>
@@ -11,6 +30,12 @@ function App() {
                     <h1 className="pokedex-title">Pokédex</h1>
                 </header>
 
+                <ul>
+                    <li>
+
+                    </li>
+                </ul>
+
                 <section className="pokemon-card-overview">
                     <PokemonCard name="ditto"/>
                     <PokemonCard name="jigglypuff"/>
@@ -18,6 +43,14 @@ function App() {
                     <PokemonCard name="bulbasaur"/>
 
                 </section>
+
+                <ul>
+                    <li className="pokemon-name">
+                        {pokemonList.map((pokemon) => (
+                            <li key={pokemon.id}>{pokemon.name}</li>
+                        ))}
+                    </li>
+                </ul>
             </main>
         </>
     )
--- src/components/PokemonCard/PokemonCard.jsx
+++ src/components/PokemonCard/PokemonCard.jsx
@@ -40,31 +40,31 @@ function PokemonCard({name}) {
     return (
         <>
             <article className="pokemon-card">
-                <h1 className="pokemon-name">{pokemon.name}</h1>
+                <h2 className="pokemon-name">{pokemon.name}</h2>
                 <img
                     src={pokemon.sprites?.front_default}
                     alt={pokemon.name}
                     width="180"
                 />
-                <p className="pokemon-properties-title">
+                <div className="pokemon-properties-title">
                     moves:
                     <div className="pokemon-property"> {pokemon.moves ? pokemon.moves.length : 0}</div>
-                </p>
-                <p className="pokemon-properties-title">
+                </div>
+                <div className="pokemon-properties-title">
                     weight:
                     <div className="pokemon-property"> {pokemon.weight}</div>
-                </p>
-                <p className="abilities">
+                </div>
+                <div className="abilities">
                     Abilities:
                     <ul className="pokemon-abilities-list"> {pokemon.abilities ? pokemon.abilities.slice(0, 3).map((abilityObj) => (
-                            <li className="pokemon-ability" key='abilityObj.ability.name'>
-                            {abilityObj.ability.name}
+                            <li className="pokemon-ability" key={pokemon.id}>
+                                {abilityObj.ability.name}
                                 {" "}
-                        </li>
+                            </li>
                         ))
-                        : <li>Geen abilities gevonden</li> }
+                        : <li>Geen abilities gevonden</li>}
                     </ul>
-                </p>
+                </div>
             </article>
         </>
     );
