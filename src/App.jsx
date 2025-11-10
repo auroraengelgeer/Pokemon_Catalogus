3f5eb8f82591bdf7297b579e886de3a559e7fdc3
auroraengelgeer <auroraengelgeer@hotmail.com>
2025-11-10 22:03:27 +0100

    added next and previous buttons

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,16 @@ import axios from "axios";
 function App() {
 
     const [pokemonList, setPokemonList] = useState([]);
+    const [nextUrl, setNextUrl] = useState(null);
+    const [previousUrl, setPreviousUrl] = useState(null);
 
     useEffect(() => {
         async function getPokemonData() {
             try {
                 const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0');
-                setPokemonList(response.data.results)
+                setPokemonList(response.data.results);
+                setNextUrl(response.data.next)
+                setPreviousUrl(response.data.previous)
             } catch (error) {
                 console.error(error)
             }
@@ -22,6 +26,28 @@ function App() {
 
     }, []);
 
+    async function fetchNextPage() {
+        try {
+            const response = await axios.get(nextUrl)
+            setPokemonList(response.data.results);
+            setNextUrl(response.data.next)
+            setPreviousUrl(response.data.previous)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    async function fetchPreviousPage() {
+        try {
+            const response = await axios.get(previousUrl)
+            setPokemonList(response.data.results);
+            setNextUrl(response.data.next)
+            setPreviousUrl(response.data.previous)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
 
     return (
         <>
@@ -29,10 +55,17 @@ function App() {
                 <header>
                     <h1 className="pokedex-title">Pokédex</h1>
                 </header>
-
+                <div className="page-button-container">
+                    <button className="page-button" type="button" onClick={fetchPreviousPage}
+                            disabled={!previousUrl}>Previous
+                    </button>
+                    <button className="page-button" type="button" onClick={fetchNextPage}
+                            disabled={!nextUrl}>Next
+                    </button>
+                </div>
                 <section className="pokemon-card-overview">
                     {pokemonList.map((pokemon) => (
-                        <PokemonCard key={pokemon.name} name={pokemon.name} />
+                        <PokemonCard key={pokemon.name} name={pokemon.name}/>
                     ))}
 
                 </section>
